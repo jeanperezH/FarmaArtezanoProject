11a10e8e06355b4500b5893bbd27e59e301ef28b
Jean Perez <jeanperezhilario@gmail.com>
2024-03-16 13:19:47 -0500

    Categorias

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\CategoriaController;
 
 Route::get('/', function () {
     return view('contenido/contenido');
 });
+
+Route::get('/categorias', [CategoriaController::class, 'index']);
+Route::post('/categorias/registrar', [CategoriaController::class, 'store']);
+Route::put('/categorias/actualizar', [CategoriaController::class, 'update']);
+Route::put('/categorias/desactivar', [CategoriaController::class, 'desactivar']);
+Route::put('/categorias/activar', [CategoriaController::class, 'activar']);
